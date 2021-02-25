bfe01ef33887ffe9d1b744d40c473aa0f6bb295c
Rashmi Mishra <rashmimishra745@gmail.com>
2021-02-25 20:54:55 +0530

    Initial commit

---
+++ SpringBoot_Technoholics_App/src/main/java/com/db/hack/SpringCloudVisionApiDemo/SpringCloudVisionApiDemoApplication.java
@@ -0,0 +1,14 @@
+package com.db.hack.SpringCloudVisionApiDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudVisionApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudVisionApiDemoApplication.class, args);
+		
+	}
+
+}
---
+++ SpringBoot_Technoholics_App/src/test/java/com/db/hack/SpringCloudVisionApiDemo/SpringCloudVisionApiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.db.hack.SpringCloudVisionApiDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudVisionApiDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
